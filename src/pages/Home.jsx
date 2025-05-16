ab8d3c947e31ec52e4c208e50404f909ebe8cb30
jamtty <jamtty1004@gmail.com>
2025-05-16 15:22:11 +0900

    현재 내용 배포

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="container">
-      <p className="text-center mt-2">메인 페이지입니다.<br />메인 페이지 입니다.</p>
+      <p className="text-center mt-2">메인 페이지입니다.<br />메인 페이지 입니다.<br />메인 페이지 입니다.</p>
     </div>
   );
 };

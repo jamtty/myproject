659a16673086386ec4b00cb9c5bf53ee54222327
jamtty <jamtty1004@gmail.com>
2025-05-16 15:53:47 +0900

    Home 페이지 내용 업데이트

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="container">
-      <p className="text-center mt-2">메인 페이지입니다.<br />메인 페이지 입니다.<br />메인 페이지 입니다.<br />자동 배포 테스트</p>
+      <p className="text-center mt-2">
+		메인 페이지입니다.<br />
+		메인 페이지 입니다.<br />
+		메인 페이지 입니다.<br />
+		자동 배포 테스트<br />
+		자동 배포 테스트2</p>
     </div>
   );
 };

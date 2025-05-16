93b91122192fa2bc5e96d052e63197a827dbcb76
jamtty <jamtty1004@gmail.com>
2025-05-16 15:58:43 +0900

    빌드 및 배포

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="container">
-      <p className="text-center mt-2">
-		메인 페이지입니다.<br />
-		메인 페이지 입니다.<br />
-		메인 페이지 입니다.<br />
-		자동 배포 테스트<br />
-		자동 배포 테스트2</p>
+		<p className="text-center mt-2">
+			메인 페이지입니다.<br />
+			메인 페이지 입니다.<br />
+			메인 페이지 입니다.<br />
+			자동 배포 테스트<br />
+			자동 배포 테스트2<br />
+			자동 배포 테스트3
+		</p>
     </div>
   );
 };
